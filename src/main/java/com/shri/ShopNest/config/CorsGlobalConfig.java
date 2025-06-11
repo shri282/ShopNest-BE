9236aa10483b10a26a2a96b350f004d09a41252f
shrikadhir <shrikadhir10@gmail.com>
2025-06-12 00:49:42 +0530

    env for fn url

--- src/main/java/com/shri/ShopNest/config/CorsGlobalConfig.java
+++ src/main/java/com/shri/ShopNest/config/CorsGlobalConfig.java
@@ -1,5 +1,6 @@
 package com.shri.ShopNest.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsGlobalConfig {
 
+    @Value("${frontend.url}")
+    private static String FE_URL;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(FE_URL)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);

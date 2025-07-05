5217ce438ecc59f493f6397b1f7ec38b6717eafb
shrikadhir <shrikadhir10@gmail.com>
2025-07-05 18:58:44 +0530

    changes

--- src/main/java/com/shri/ShopNest/exception/ApiException.java
+++ src/main/java/com/shri/ShopNest/exception/ApiException.java
@@ -9,12 +9,10 @@ import org.springframework.http.HttpStatus;
 public class ApiException extends RuntimeException {
     private final HttpStatus status;
     private final String errorCode;
-    private final String message;
 
     public ApiException(HttpStatus status, String errorCode, String message) {
         super(message);
         this.status = status;
         this.errorCode = errorCode;
-        this.message = message;
     }
 }

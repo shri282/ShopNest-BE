69dfb72d4712be70137580a7db66ebba1daea0ad
shri kadhir <shrikadhir.k@tolaram.com>
2025-11-02 00:27:22 +0530

    oauth changes 2

--- src/main/java/com/shri/ShopNest/modules/oauth/GoogleOAuthImpl.java
+++ src/main/java/com/shri/ShopNest/modules/oauth/GoogleOAuthImpl.java
@@ -14,9 +14,10 @@ public class GoogleOAuthImpl implements OAuth {
     @Override
     public String getLoginUrl() {
         return "https://accounts.google.com/o/oauth2/v2/auth" +
-                "?client_id=" +
-                googleOAuthProperties.getClientId() +
-                "&redirect_uri=" +
-                googleOAuthProperties.getRedirectUri();
+                "?client_id=" + googleOAuthProperties.getClientId() +
+                "&redirect_uri=" + googleOAuthProperties.getRedirectUri() +
+                "&response_type=code" +
+                "&scope=openid%20email%20profile" +
+                "&access_type=offline";
     }
 }
